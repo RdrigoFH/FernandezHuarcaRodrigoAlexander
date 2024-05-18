8e416253e1e00260a4887725fcca73edb343be1b
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-18 15:34:54 -0500

    Se agrego funcionalidad en history

--- Laboratorio03/Ejercicio02/index.js
+++ Laboratorio03/Ejercicio02/index.js
@@ -1,8 +1,10 @@
 const display = document.getElementById("display");
+const history = document.getElementById("history")
 
 function show(input){
    display.value += input;
 }
 function calculate(){
+   history.textContent += display.value + "= " + eval(display.value);
    display.value = eval(display.value);
 }
