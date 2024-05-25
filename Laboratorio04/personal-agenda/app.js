a10a36715873ff1f68c7b9c3349ec069dbf5248d
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-25 12:46:39 -0500

    Se agregaron gets para el index, agregar y editar

--- Laboratorio04/personal-agenda/app.js
+++ Laboratorio04/personal-agenda/app.js
@@ -9,3 +9,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let events = require('./events.json');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'add.html'));
+});
+
+app.get('/edit/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'edit.html'));
+});
+
+app.get('/api/events', (req, res) => {
+    res.json(events);
+});
+
