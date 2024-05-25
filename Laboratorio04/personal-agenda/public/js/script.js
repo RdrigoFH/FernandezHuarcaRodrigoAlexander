d8c2ac56084cbb24c207a7b200622ea2beabe225
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-25 12:33:07 -0500

    se usa la funcion fetch para administrar los eventos y crearlos

--- Laboratorio04/personal-agenda/public/js/script.js
+++ Laboratorio04/personal-agenda/public/js/script.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
    const eventsList = document.getElementById('eventsList');
 
+   if (eventsList) {
+       fetch('/api/events')
+           .then(response => response.json())
+           .then(events => {
+               events.forEach(event => {
+                   const li = document.createElement('li');
+                   li.innerHTML = `
+                       <strong>${event.date} ${event.time} - ${event.title}</strong><br>
+                       ${event.description}
+                       <form action="#" method="POST" class="deleteForm" data-id="${event.id}">
+                           <button type="submit">Delete</button>
+                       </form>
+                       <a href="edit.html?id=${event.id}">Edit</a>
+                   `;
+                   eventsList.appendChild(li);
+               });
 
 
 
